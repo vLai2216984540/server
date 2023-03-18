8faee0c4abc82cab27303405c0171939adc8d0ef
VLai <fl2216984540@163.com>
2023-03-18 20:52:43 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const { request } = require("express");
+const express = require("express"); //导入了Node内置的网络服务器模块
+const app = express();
+const cors = require("cors");
+app.use(express.json()); //将请求的json数据转化为JavaScript对象，然后在调用路由处理程序之前将其附加到request对象的body属性
+app.use(cors()); //允许来自其他原点的请求
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2023-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<H1>Hello World!</H1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end(); // 204表示无内容
+});
+
+const generateId = () => {
+  const MaxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return MaxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
